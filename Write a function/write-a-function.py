dbeeaa898c01368ba1982d052f004fbd2dffb591
Srisabarivasan <97931198+srisabarivasan@users.noreply.github.com>
2024-07-30 22:50:54 +0530

    Update write-a-function.py

--- Write a function/write-a-function.py
+++ Write a function/write-a-function.py
@@ -2,7 +2,7 @@ def is_leap(year):
     if year % 400 == 0: 
         return True 
     elif year % 100 == 0: 
-        return True 
+        return False 
     elif year % 4 == 0: 
         return True 
     else: 
